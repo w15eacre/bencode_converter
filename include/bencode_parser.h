c4b1315ff1536ffd9733e95353129c5513c72fc2
Hovard Smith <oleg.kuznetsov@internet.ru>
2023-11-23 03:54:32 +0300

    Refactoring the exeption system (#4)
    * Added .clang-format
    
    * Refactoring the exeption system
    
    - Add the fmt library
    - Add nested exeptions
    
    * Fix the CI script

--- include/bencode_parser.h
+++ include/bencode_parser.h
@@ -1,22 +1,159 @@
 #pragma once
 
-#include <algorithm>
-#include <cctype>
+#include <fmt/format.h>
+
 #include <charconv>
 #include <concepts>
 #include <exception>
 #include <map>
-#include <string>
-#include <string_view>
-#include <type_traits>
 #include <variant>
 #include <vector>
 
 namespace converter::bencode {
 
-namespace details {
+template <typename... Args>
+constexpr std::convertible_to<std::string_view> auto Format(fmt::format_string<Args...> pattern, Args&&... args)
+{
+    return fmt::format(pattern, std::forward<Args>(args)...);
+}
+
+namespace type_traits {
 
 constexpr char InvalidSymbol = '\0';
+
+template <typename T>
+concept BencodeTypeConcept = requires(T bencode) {
+    typename T::Int;
+    typename T::Str;
+    typename T::List;
+    typename T::Dict;
+
+    typename T::Variant;
+
+    requires std::convertible_to<T, typename T::Variant>;
+};
+
+template <typename Token>
+concept HasTokenType = requires { typename Token::Type; };
+
+template <typename Token>
+concept HasTokenValue = requires(Token token) { requires std::same_as<decltype(token.token), char>; };
+
+template <typename Token>
+concept TokenConcept = requires {
+    typename Token::BencodeType;
+
+    std::declval<Token>() == InvalidSymbol;
+    InvalidSymbol == std::declval<Token>();
+};
+
+template <typename TypeTraits>
+concept TypeTraitsConcept = requires {
+    {
+        TypeTraits::GetIntToken()
+    } -> TokenConcept;
+    {
+        TypeTraits::GetListToken()
+    } -> TokenConcept;
+
+    {
+        TypeTraits::GetDictToken()
+    } -> TokenConcept;
+
+    {
+        TypeTraits::GetSepToken()
+    } -> TokenConcept;
+
+    {
+        TypeTraits::GetStrToken()
+    } -> TokenConcept;
+};
+
+template <BencodeTypeConcept B, typename T>
+struct Token
+{
+    using BencodeType = B;
+    using Type = T;
+    const char Token = InvalidSymbol;
+
+    bool operator==(char ch) const noexcept
+    {
+        return ch == Token;
+    }
+};
+
+template <BencodeTypeConcept BencodeType>
+struct BencodeTypeTraits;
+
+template <BencodeTypeConcept B>
+struct Token<B, typename BencodeTypeTraits<B>::NoTypeToken>
+{
+    using BencodeType = B;
+    const char Token = InvalidSymbol;
+
+    bool operator==(char ch) const noexcept
+    {
+        return Token == ch;
+    }
+};
+
+template <BencodeTypeConcept B>
+struct Token<B, typename BencodeTypeTraits<B>::StrType>
+{
+    using BencodeType = B;
+    using Type = std::string;
+
+    bool operator==(char ch) const noexcept
+    {
+        return std::isdigit(ch);
+    }
+};
+
+template <BencodeTypeConcept BencodeType>
+struct BencodeTypeTraits
+{
+    using IntType = BencodeType::Int;
+    using StrType = BencodeType::Str;
+    using ListType = BencodeType::List;
+    using DictType = BencodeType::Dict;
+    using NoTypeToken = void;
+
+    using Variant = std::variant<IntType, StrType, ListType, DictType>;
+
+    constexpr static Token<BencodeType, NoTypeToken> GetEndToken() noexcept
+    {
+        return {'e'};
+    }
+
+    constexpr static Token<BencodeType, IntType> GetIntToken() noexcept
+    {
+        return {'i'};
+    }
+
+    constexpr static Token<BencodeType, ListType> GetListToken() noexcept
+    {
+        return {'l'};
+    };
+
+    constexpr static Token<BencodeType, DictType> GetDictToken() noexcept
+    {
+        return {'d'};
+    }
+
+    constexpr static Token<BencodeType, NoTypeToken> GetSepToken() noexcept
+    {
+        return {':'};
+    }
+
+    constexpr static Token<BencodeType, StrType> GetStrToken() noexcept
+    {
+        return {};
+    }
+};
+
+} // namespace type_traits
+
+namespace details {
 
 template <typename I, typename S, template <typename...> typename L, template <typename...> typename D>
 class BencodeType
@@ -48,193 +185,75 @@ private:
     std::variant<Int, Str, List, Dict> m_variant{};
 };
 
-} // namespace details
-
-namespace type_traits {
-
-template <typename T>
-concept BencodeTypeConcept = requires(T bencode) {
-    typename T::Int;
-    typename T::Str;
-    typename T::List;
-    typename T::Dict;
-
-    typename T::Variant;
-
-    requires std::convertible_to<T, typename T::Variant>;
-};
-
-template <typename Token>
-concept HasTokenType = requires { typename Token::Type; };
-
-template <typename Token>
-concept HasTokenValue = requires(Token token) { requires std::same_as<decltype(token.token), char>; };
-
-template <typename Token>
-concept TokenConcept = requires {
-    typename Token::BencodeType;
-
-    std::declval<Token>() == details::InvalidSymbol;
-    details::InvalidSymbol == std::declval<Token>();
-};
-
-template <typename TypeTraits>
-concept TypeTraitsConcept = requires {
-    {
-        TypeTraits::GetIntToken()
-    } -> TokenConcept;
-    {
-        TypeTraits::GetListToken()
-    } -> TokenConcept;
-
-    {
-        TypeTraits::GetDictToken()
-    } -> TokenConcept;
-
-    {
-        TypeTraits::GetSepToken()
-    } -> TokenConcept;
-
-    {
-        TypeTraits::GetStrToken()
-    } -> TokenConcept;
-};
-
-template <BencodeTypeConcept B, typename T>
-struct Token
-{
-    using BencodeType = B;
-    using Type = T;
-    const char token = details::InvalidSymbol;
-
-    bool operator==(char ch) const noexcept
-    {
-        return ch == token;
-    }
-};
-
-template <BencodeTypeConcept BencodeType>
-struct BencodeTypeTraits;
-
-template <BencodeTypeConcept B>
-struct Token<B, typename BencodeTypeTraits<B>::NoTypeToken>
-{
-    using BencodeType = B;
-    const char token = details::InvalidSymbol;
-
-    bool operator==(char ch) const noexcept
-    {
-        return token == ch;
-    }
-};
-
-template <BencodeTypeConcept B>
-struct Token<B, typename BencodeTypeTraits<B>::StrType>
-{
-    using BencodeType = B;
-    using Type = std::string;
-
-    bool operator==(char ch) const noexcept
-    {
-        return std::isdigit(ch);
-    }
-};
-
-template <BencodeTypeConcept BencodeType>
-struct BencodeTypeTraits
-{
-    using IntType = BencodeType::Int;
-    using StrType = BencodeType::Str;
-    using ListType = BencodeType::List;
-    using DictType = BencodeType::Dict;
-    using NoTypeToken = void;
-
-    using Variant = std::variant<IntType, StrType, ListType, DictType>;
-
-    static Token<BencodeType, NoTypeToken> GetEndToken() noexcept
-    {
-        return {'e'};
-    }
-
-    static Token<BencodeType, IntType> GetIntToken() noexcept
-    {
-        return {'i'};
-    }
-
-    static Token<BencodeType, ListType> GetListToken() noexcept
-    {
-        return {'l'};
-    };
-
-    static Token<BencodeType, DictType> GetDictToken() noexcept
-    {
-        return {'d'};
-    }
-
-    static Token<BencodeType, NoTypeToken> GetSepToken() noexcept
-    {
-        return {':'};
-    }
-
-    static Token<BencodeType, StrType> GetStrToken() noexcept
-    {
-        return {};
-    }
-};
-
-} // namespace type_traits
-
-namespace details {
 template <type_traits::BencodeTypeConcept T, std::forward_iterator It>
 std::pair<It, typename type_traits::BencodeTypeTraits<T>::Variant> ParseInt(It begin, It end)
 {
-    if (*begin != type_traits::BencodeTypeTraits<T>::GetIntToken())
+    try
     {
-        // throw std::invalid_argument(std::format("Expected the bencode format: i<number>e, actualy - {}", std::string_view(begin, end)));
-        throw std::invalid_argument("Expected the bencode format: i<number>e, actualy"); // Compatibility with GCC 11
+        constexpr type_traits::TokenConcept auto IntToken = type_traits::BencodeTypeTraits<T>::GetIntToken();
+        if (*begin != IntToken)
+        {
+            throw std::invalid_argument(Format("Expected the first letter '{}', actually: {}", IntToken.Token, *begin));
+        }
+
+        constexpr auto IsDigit = [](auto ch) -> bool {
+            return std::isdigit(ch);
+        };
+
+        auto firstDigit = std::find_if(begin, end, IsDigit);
+        if (firstDigit == end)
+        {
+            throw std::invalid_argument("Expected at least one digit, actualy - digit not found");
+        }
+
+        typename type_traits::BencodeTypeTraits<T>::IntType result{};
+        auto [it, error] = std::from_chars(firstDigit, end, result);
+        if (error == std::errc() && it != end && *it == type_traits::BencodeTypeTraits<T>::GetEndToken())
+        {
+            return std::make_pair(std::next(it), T{std::move(result)});
+        }
+
+        throw std::system_error(std::make_error_code(error));
     }
-
-    constexpr auto IsDigit = [](auto ch) -> bool {
-        return std::isdigit(ch);
-    };
-
-    auto firstDigit = std::find_if(begin, end, IsDigit);
-    if (firstDigit == end)
+    catch (...)
     {
-        // throw std::invalid_argument(
-        // std::format("Expected at least one digit, actualy - digit not found - {}", std::string_view(begin, end)));
-        throw std::invalid_argument("Expected at least one digit, actualy - digit not found"); // Compatibility with GCC 11
+        std::throw_with_nested(
+            std::runtime_error(Format("{}:{}:Failed to parse int value: {}", __FUNCTION__, __LINE__, std::string_view(begin, end))));
     }
-
-    typename type_traits::BencodeTypeTraits<T>::IntType result{};
-    auto [it, error] = std::from_chars(firstDigit, end, result);
-    if (error == std::errc() && it != end && *it == type_traits::BencodeTypeTraits<T>::GetEndToken())
-    {
-        return std::make_pair(std::next(it), T{std::move(result)});
-    }
-
-    throw std::system_error(static_cast<int>(error), std::system_category());
 }
 
 template <type_traits::BencodeTypeConcept T, std::forward_iterator It>
 std::pair<It, typename type_traits::BencodeTypeTraits<T>::Variant> ParseString(It begin, It end)
 {
-    size_t sizeOf{};
-    auto [sepIt, error] = std::from_chars(begin, end, sizeOf);
-
-    if (error != std::errc())
+    try
     {
-        throw std::system_error(static_cast<int>(error), std::system_category());
-    }
+        size_t sizeOf{};
+        auto [sepIt, error] = std::from_chars(begin, end, sizeOf);
 
-    if (*sepIt != type_traits::BencodeTypeTraits<T>::GetSepToken())
+        if (error != std::errc())
+        {
+            throw std::system_error(std::make_error_code(error));
+        }
+
+        if (sepIt == end)
+        {
+            throw std::invalid_argument("Separate token not found");
+        }
+
+        constexpr type_traits::TokenConcept auto SepToken = type_traits::BencodeTypeTraits<T>::GetSepToken();
+        if (*sepIt != SepToken)
+        {
+            throw std::invalid_argument(Format("Expected '{}' token, actually: {}", SepToken.Token, *sepIt));
+        }
+
+        auto endIt = std::next(sepIt, sizeOf + 1);
+
+        return std::make_pair(endIt, typename type_traits::BencodeTypeTraits<T>::StrType{std::next(sepIt), endIt});
+    }
+    catch (...)
     {
-        throw std::system_error(static_cast<int>(error), std::system_category());
+        std::throw_with_nested(std::runtime_error(Format("{}:{}:Failed to parse string value: {}", __FUNCTION__, __LINE__, std::string_view(begin, end))));
     }
-
-    auto endIt = std::next(sepIt, sizeOf + 1);
-
-    return std::make_pair(endIt, typename type_traits::BencodeTypeTraits<T>::StrType{std::next(sepIt), endIt});
 }
 
 template <type_traits::BencodeTypeConcept T, std::forward_iterator It>
@@ -243,49 +262,74 @@ std::pair<It, typename type_traits::BencodeTypeTraits<T>::Variant> Parse(It begi
 template <type_traits::BencodeTypeConcept T, std::forward_iterator It>
 std::pair<It, typename type_traits::BencodeTypeTraits<T>::Variant> ParseList(It begin, It end)
 {
-    if (*begin != type_traits::BencodeTypeTraits<T>::GetListToken())
+    try
     {
-        //throw std::invalid_argument(std::format("Expected the bencode format: i<number>e, actualy - {}", std::string_view(begin, end)));
-        throw std::invalid_argument("Expected the bencode format: i<number>e, actualy"); // Compatibility with GCC 11
+        constexpr type_traits::TokenConcept auto ListToken = type_traits::BencodeTypeTraits<T>::GetListToken();
+        if (*begin != ListToken)
+        {
+            throw std::invalid_argument(
+                Format("Expected the first letter '{}', actually {}", ListToken.Token, std::string_view(begin, end)));
+        }
+
+        typename type_traits::BencodeTypeTraits<T>::ListType list{};
+
+        auto outputIt = std::back_inserter(list);
+
+        auto it = std::next(begin);
+        if (it == end)
+        {
+            throw std::invalid_argument("The list ended unexpectedly");
+        }
+
+        constexpr type_traits::TokenConcept auto EndToken = type_traits::BencodeTypeTraits<T>::GetEndToken();
+        while (*it != EndToken)
+        {
+            auto [endIt, value] = Parse<T>(it, end);
+            *outputIt = std::move(value);
+            it = endIt;
+        }
+
+        return std::make_pair(end, T{list});
     }
-
-    typename type_traits::BencodeTypeTraits<T>::ListType list{};
-
-    auto outputIt = std::back_inserter(list);
-
-    auto it = std::next(begin);
-    while (*it != type_traits::BencodeTypeTraits<T>::GetEndToken())
+    catch (...)
     {
-        auto [endIt, value] = Parse<T>(it, end);
-        *outputIt = std::move(value);
-        it = endIt;
+        std::throw_with_nested(std::runtime_error(Format("{}:{}:Failed to parse list value: {}", __FUNCTION__, __LINE__, std::string_view(begin, end))));
     }
-
-    return std::make_pair(end, T{list});
 }
 
 template <type_traits::BencodeTypeConcept T, std::forward_iterator It>
 std::pair<It, typename type_traits::BencodeTypeTraits<T>::Variant> Parse(It begin, It end)
 {
-    if (*begin == type_traits::BencodeTypeTraits<T>::GetIntToken())
+    try
     {
-        return ParseInt<T>(begin, end);
-    }
-    else if (*begin == type_traits::BencodeTypeTraits<T>::GetListToken())
-    {
-        return ParseList<T>(begin, end);
-    }
-    else if (*begin == type_traits::BencodeTypeTraits<T>::GetDictToken())
-    {
-        std::terminate();
-    }
-    else if (*begin == type_traits::BencodeTypeTraits<T>::GetStrToken())
-    {
-        return ParseString<T>(begin, end);
-    }
+        if (begin == end)
+        {
+            throw std::invalid_argument("Empty string");
+        }
 
-    //throw std::invalid_argument(std::format("Unexpected symbol - {}", *begin));
-    throw std::invalid_argument("Unexpected symbol"); // Compatibility with GCC 11
+        if (*begin == type_traits::BencodeTypeTraits<T>::GetIntToken())
+        {
+            return ParseInt<T>(begin, end);
+        }
+        else if (*begin == type_traits::BencodeTypeTraits<T>::GetListToken())
+        {
+            return ParseList<T>(begin, end);
+        }
+        else if (*begin == type_traits::BencodeTypeTraits<T>::GetDictToken())
+        {
+            std::terminate(); // Hasn't been unimplemented yet.
+        }
+        else if (*begin == type_traits::BencodeTypeTraits<T>::GetStrToken())
+        {
+            return ParseString<T>(begin, end);
+        }
+
+        throw std::invalid_argument(Format("Unexpected symbol: {}", *begin));
+    }
+    catch (...)
+    {
+        std::throw_with_nested(std::runtime_error(Format("{}:{}:Failed to parse Bencode value: {}", __FUNCTION__, __LINE__, std::string_view(begin, end))));
+    }
 }
 
 } // namespace details
@@ -298,11 +342,16 @@ using BenCodeVariantView = type_traits::BencodeTypeTraits<BaseTypeView>::Variant
 template <type_traits::BencodeTypeConcept T>
 type_traits::BencodeTypeTraits<T>::Variant Parse(std::string_view data)
 {
+    if (data.empty())
+    {
+        throw std::invalid_argument("Failed to parse bencode value: passed empty value");
+    }
+
     auto [it, value] = details::Parse<T>(std::cbegin(data), std::cend(data));
     if (it != std::cend(data))
     {
-        //throw std::invalid_argument(std::format("Failed to parse bencode: ", data));
-        throw std::invalid_argument("Failed to parse bencode"); // Compatibility with GCC 11
+        throw std::runtime_error(
+            Format("Failed to parse bencode value: Unparsed data starts with {}: {}", std::distance(data.cbegin(), it), data));
     }
 
     return value;
